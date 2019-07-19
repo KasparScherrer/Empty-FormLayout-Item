0104c18fde32c8f01dbe280692514a9efdbf5811
kscherrer <kaspar.scherrer@car-net.com>
2019-07-19 10:08:41 +0200

    drastically improved the demo view

--- src/test/java/ch/carnet/kasparscherrer/DemoView.java
+++ src/test/java/ch/carnet/kasparscherrer/DemoView.java
@@ -7,23 +7,41 @@ import com.vaadin.flow.router.Route;
 
 @Route("")
 public class DemoView extends Div {
+    private TextField textField1, textField2, textField3, textField4;
 
-    public DemoView() {
+    public DemoView(){
+        addLayout();
+    }
+
+    private void addLayout() {
+        initializeTextFields();
+
         FormLayout formLayout = new FormLayout();
         formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 2));
-        formLayout.setWidth("100%");
-
-        TextField testField = new TextField("foo");
-        testField.setPlaceholder("bar");
-        EmptyFormLayoutItem emptyItem = new EmptyFormLayoutItem();
-
+        formLayout.setWidth("500px");
+        formLayout.getStyle().set("border", "1px solid black");
         formLayout.add(
-                testField,      testField,
-                emptyItem,      testField,
-                emptyItem,      emptyItem,
-                testField,      emptyItem
+                textField1,                 textField2,
+                textField3,                 new EmptyFormLayoutItem(),
+                new EmptyFormLayoutItem(),  new EmptyFormLayoutItem(),
+                new EmptyFormLayoutItem(),  textField4
         );
 
         add(formLayout);
+    }
+
+    private void initializeTextFields() {
+        textField1 = new TextField("foo");
+        textField1.setPlaceholder("bar");
+        textField1.setWidth("100%");
+        textField2 = new TextField("foo");
+        textField2.setPlaceholder("bar");
+        textField2.setWidth("100%");
+        textField3 = new TextField("foo");
+        textField3.setPlaceholder("bar");
+        textField3.setWidth("100%");
+        textField4 = new TextField("foo");
+        textField4.setPlaceholder("bar");
+        textField4.setWidth("100%");
     }
 }
